459ff482fc4189ee91335d1b8c219c1e611c7db9
BugHunter-X <askzenai@gmail.com>
2025-02-23 21:44:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.writeHead(200);
+  response.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080);
+console.log('Server is running on port 8080');
+//Uncommon bug: Omitting error handling in the server
+//The server is running but there is no handling for errors.
